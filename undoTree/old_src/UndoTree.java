0fdc4caccd80cf3216575157e2b05127e835297e
Michael Scheer <mgscheer@newpembroke1-4-65.resnet.brown.edu>
2013-04-12 02:31:31 -0400

    undoTree infrastructure working plus a gui that demonstrates part of it.

---
+++ undoTree/old_src/UndoTree.java
@@ -0,0 +1,19 @@
+package bruno.undoTree;
+
+import java.util.*;
+import java.io.*;
+import javax.swing.text.*;
+import javax.swing.undo.*;
+
+public class UndoTree implements DocumentListener
+{
+    //    private List<UndoableEditEvent> events;
+    
+    @Override
+	public void changedUpdate(DocumentEvent e)
+    {
+
+    }
+
+
+}
