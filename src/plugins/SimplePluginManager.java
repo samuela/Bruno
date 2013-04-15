78e7af8355d20f5fd0f32c4ade43d29ca06c9523
Samuel Ainsworth <samuel_ainsworth@brown.edu>
2013-04-14 22:47:20 -0400

    Add Apache Commons and Google Guava

--- src/plugins/SimplePluginManager.java
+++ src/plugins/SimplePluginManager.java
@@ -1,92 +1,104 @@
 package plugins;
 
-import org.apache.commons.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 
-import javax.script.*;
-import java.io.*;
-import java.util.*;
 /**
- * Created with IntelliJ IDEA.
- * User: jonathan
- * Date: 4/10/13
- * Time: 10:29 PM
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: jonathan Date: 4/10/13 Time: 10:29 PM To
+ * change this template use File | Settings | File Templates.
  */
-public class SimplePluginManager implements PluginManager{
+public class SimplePluginManager implements PluginManager {
 
-    private ScriptEngineManager factory_;
-    private Map<String, ScriptEngine> enginesByExtension_;
-    private Map<Script, FileReader> scriptFileReaders_;
+	private ScriptEngineManager factory_;
+	private Map<String, ScriptEngine> enginesByExtension_;
+	private Map<Script, FileReader> scriptFileReaders_;
 
-    public SimplePluginManager(){
-        factory_ = new ScriptEngineManager();
-        enginesByExtension_ = new HashMap<String, ScriptEngine>();
-        scriptFileReaders_ = new HashMap<Script, FileReader>();
+	public SimplePluginManager() {
+		factory_ = new ScriptEngineManager();
+		enginesByExtension_ = new HashMap<String, ScriptEngine>();
+		scriptFileReaders_ = new HashMap<Script, FileReader>();
+	}
 
-    }
+	public boolean hasEngine(String extension) {
+		return enginesByExtension_.containsKey(extension);
+	}
 
-    public boolean hasEngine(String extension){
-        return enginesByExtension_.containsKey(extension);
-    }
+	public boolean supportsScript(Script s) {
+		return scriptFileReaders_.containsKey(s) && hasEngine(s.getExtension());
+	}
 
-    public boolean supportsScript(Script s){
-        return scriptFileReaders_.containsKey(s) && hasEngine(s.getExtension());
-    }
+	// a script cannot be executed unless its plugin has been loaded, so it is
+	// safe to assume:
+	// 1) engine for extension exists
+	// 2) file for script exists
+	@Override
+	public void executeScript(Script userScript) throws ScriptException {
+		// System.out.println("getting engine for " +
+		// userScript.getExtension());
+		ScriptEngine engine = enginesByExtension_
+				.get(userScript.getExtension());
+		// System.out.println("ext:" + userScript.getExtension());
+		// System.out.println("contains py:" +
+		// enginesByExtension_.containsKey("py"));
+		try {
+			// System.out.println("engine null: " + (engine == null));
+			engine.eval(scriptFileReaders_.get(userScript));
+		} catch (ScriptException e) {
+			throw new ScriptException("Error in Script: " + userScript);
+		}
+	}
 
-    //a script cannot be executed unless its plugin has been loaded, so it is safe to assume:
-    //1) engine for extension exists
-    //2) file for script exists
-    @Override
-    public void executeScript(Script userScript) throws ScriptException {
-    //    System.out.println("getting engine for " + userScript.getExtension());
-        ScriptEngine engine = enginesByExtension_.get(userScript.getExtension());
-    //    System.out.println("ext:" + userScript.getExtension());
-    //    System.out.println("contains py:"  + enginesByExtension_.containsKey("py"));
-        try {
-     //       System.out.println("engine null: " + (engine == null));
-            engine.eval(scriptFileReaders_.get(userScript));
-        } catch (ScriptException e) {
-            throw new ScriptException("Error in Script: " + userScript);
-        }
-    }
+	// if plugin contains any new scripting languages, create new engines for
+	// them
+	// open new FileReader for each script in plugin recursively
+	@Override
+	public Plugin loadPlugin(File directoryPath) {
+		Plugin plugin = new Plugin(directoryPath.getAbsolutePath());
+		Collection<File> files = FileUtils.listFiles(directoryPath,
+				TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+		for (File f : files) {
+			String path = f.getAbsolutePath();
+			Script s = null;
+			try {
+				s = new Script(path, plugin);
+			} catch (IllegalArgumentException e) {
+				// invalid path - ignore file
+				continue;
+			}
+			plugin.addScript(s.getName(), s);
+			String extension = s.getExtension();
+			if (!enginesByExtension_.containsKey(extension)) {
+				// System.out.println("storing new engine for " + extension);
+				// System.out.println("getting engine:" +
+				// factory_.getEngineByExtension(extension));
+				enginesByExtension_.put(extension,
+						factory_.getEngineByExtension(extension));
+			}
+			try {
+				FileReader scriptReader = new FileReader(f);
+				scriptFileReaders_.put(s, scriptReader);
+			} catch (FileNotFoundException e) {
+				System.err.println("file " + path + " has disappeared.");
+			}
 
-    //if plugin contains any new scripting languages, create new engines for them
-    //open new FileReader for each script in plugin recursively
-    @Override
-    public Plugin loadPlugin(File directoryPath) {
-        Plugin plugin = new Plugin(directoryPath.getAbsolutePath());
-        Collection<File> files = FileUtils.listFiles(directoryPath, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
-        for(File f: files) {
-            String path = f.getAbsolutePath();
-            Script s = null;
-            try{
-               s = new Script(path, plugin);
-            } catch (IllegalArgumentException e){
-                //invalid path - ignore file
-                continue;
-            }
-            plugin.addScript(s.getName(), s);
-            String extension = s.getExtension();
-            if(!enginesByExtension_.containsKey(extension)) {
-                //      System.out.println("storing new engine for " + extension);
-         //       System.out.println("getting engine:" + factory_.getEngineByExtension(extension));
-                enginesByExtension_.put(extension, factory_.getEngineByExtension(extension));
-            }
-            try {
-                FileReader scriptReader = new FileReader(f);
-                scriptFileReaders_.put(s, scriptReader);
-            } catch (FileNotFoundException e) {
-                System.err.println("file " + path + " has disappeared.");
-            }
+		}
+		return plugin;
+	}
 
-        }
-        return plugin;
-    }
-
-    @Override
-    public LanguageBundle loadLanguageBundle(File bundle) {
-        return null;
-    }
+	@Override
+	public LanguageBundle loadLanguageBundle(File bundle) {
+		return null;
+	}
 
 }
