29baf145b730ef6d146da1c29c825843a41f79ef
Jonathan Lessinger <jonathan.lessinger@gmail.com>
2013-04-29 01:17:54 -0400

    fooable

---
+++ src/foobar/Fooable.java
@@ -0,0 +1,11 @@
+package foobar;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jonathan
+ * Date: 4/29/13
+ * Time: 12:29 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Fooable {
+}
