22ff3d34d5ff6674d3ad56d6173b35bf28a85fac
Jonathan Lessinger <jonathan.lessinger@gmail.com>
2013-04-29 01:18:33 -0400

    fooable

--- src/foobar/Fooable.java
+++ src/foobar/Fooable.java
@@ -1,5 +1,7 @@
 package foobar;
 
+import java.util.Set;
+
 /**
  * Created with IntelliJ IDEA.
  * User: jonathan
@@ -8,4 +10,9 @@ package foobar;
  * To change this template use File | Settings | File Templates.
  */
 public interface Fooable {
-}
+        public String getName(); // the name of this Fooable
+        public Set<String> getKeywords(); // these are the word by which Foobar searches for this Fooable
+        public void doAction(); // this is called when a suggestion is selected
+        public String toString(); // should return whatever you want to appear in the suggestion menu, typically getName()
+    }
+
