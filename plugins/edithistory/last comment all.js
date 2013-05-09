0d73a365ada11bc70092da3243d24e553d7cfbb5
Michael Scheer <mgscheer@gmail.com>
2013-05-09 10:50:28 -0400

    new plugin

---
+++ plugins/edithistory/last comment all.js
@@ -0,0 +1,42 @@
+importPackage(Packages.javax.swing);
+importPackage(Packages.java.io);
+importPackage(Packages.org.bruno.frontend);
+
+var rootFolder = bruno.getProjectExplorer().getCurrentFolder();
+
+if (rootFolder != null) {
+    var comment = JOptionPane.showInputDialog(bruno, "Enter comment to add:");
+
+  function process(file) {
+    if (file.isFile()) {
+      var doc = new org.bruno.frontend.DocumentModel(file);
+      if (doc.getMetadataFile().exists()) {
+        // Process a file
+        //      println(file);
+
+        var ew;
+        var currentDoc = bruno.getEditingWindow().getDoc();
+        if (currentDoc == null || currentDoc.getFile() == null || !currentDoc.getFile().equals(file)) {
+          ew = new org.bruno.frontend.EditingWindow(null, doc);
+        } else {
+          ew = bruno.getEditingWindow();
+        }
+
+        // Do the change
+        ew.getUndoController().setLastComment(comment);
+
+        // Save
+        ew.save();
+      }
+    } else {
+      var fileList = file.listFiles();
+      for (var i = 0; i < fileList.length; i++) {
+        process(fileList[i]);
+      }
+    }
+  }
+
+  process(rootFolder);
+} else {
+  JOptionPane.showMessageDialog(bruno, "Must have a working folder to revert files in!");
+}
