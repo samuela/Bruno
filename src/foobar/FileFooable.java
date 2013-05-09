592ef6d72e08059c5cdfbdb1b15b8484e2077538
Frank Goodman <frank+gh@frankgoodman.org>
2013-05-08 20:45:41 -0400

    comments and threading

--- src/foobar/FileFooable.java
+++ src/foobar/FileFooable.java
@@ -1,13 +1,14 @@
 package foobar;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import frontend.Bruno;
 
 /**
- * A Fooable for Files.
+ * A wrapper Fooable for opening files.
  * 
  * @author samuelainsworth
  * 
@@ -17,31 +18,48 @@ public class FileFooable implements Fooable {
 	private Bruno parentApp;
 	private File file;
 
+	/**
+	 * Create a Fooable that will open file in parentApp when executed.
+	 * 
+	 * @param parentApp The parent application
+	 * @param file The file to be opened
+	 */
 	public FileFooable(Bruno parentApp, File file) {
 		this.parentApp = parentApp;
 		this.file = file;
 	}
 
+	/**
+	 * @return The name of the file
+	 */
 	@Override
 	public String getName() {
 		return file.getName();
 	}
 
+	/**
+	 * @return A set of keywords for finding this Fooable
+	 */
 	@Override
 	public Set<String> getKeywords() {
-		// TODO split path and add as keywords
 		Set<String> r = new HashSet<>();
-		r.add(file.getName());
+		r.addAll(Arrays.asList(file.getName().split(".")));
+		r.addAll(Arrays.asList(file.getName().split(" ")));
 		return r;
 	}
 
+	/**
+	 * Open the file
+	 */
 	@Override
 	public void doAction() {
-		System.out.println("opening " + file);
 		parentApp.openFile(file);
 		parentApp.toggleFoobar();
 	}
 
+	/**
+	 * @return The name of this Fooable
+	 */
 	@Override
 	public String toString() {
 		return file.getName();
