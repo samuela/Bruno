aa1849c4247670279d144348b11b21e58d131e8f
Michael Scheer <mgscheer@gmail.com>
2013-05-09 19:52:07 -0400

    idk

--- plugins/config/key bindings.js
+++ plugins/config/key bindings.js
@@ -2,10 +2,16 @@ importPackage(Packages.javax.swing);
 //import some stuff
 
 //key is something like KeyEvent.VK_N for command n
+//mask is a string -- either "command" or "control"
 //name is a string
-function bindKeyToName(key, name){
-    getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(key, Toolkit
+function bindKeyToName(key, mask, name){
+    if (mask.equals("command")){
+	getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(key, Toolkit
 						.getDefaultToolkit().getMenuShortcutKeyMask()), name);
+    }
+    else if (mask.equals("control")){
+	getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(key, ActionEvent.CTRL_MASK()), name);
+    }
 }
 
 //name is a string
@@ -15,8 +21,8 @@ function bindNameToAction(name, action){
 }
 
 //foo is a fooable
-function bindKeyToFooable(key, name, foo){
-    bindKeyToName(key, name);
+function bindKeyToFooable(key, mask, name, foo){
+    bindKeyToName(key, mask, name);
     bindNameToAction(name, bruno.fooAction(foo));
 }
 
--- plugins/text/go to line.js
+++
@@ -1,9 +0,0 @@
-importPackage(Packages.javax.swing);
-
-var line = JOptionPane.showInputDialog(bruno, "Enter line number:");
-
-var textArea = bruno.getEditingWindow().getTextArea();
-if (line >= 1 && line <= textArea.getLineCount()){
-    println(line);
-    textArea.setCaretPosition(textArea.getLineStartOffset(line-1));
-} 
