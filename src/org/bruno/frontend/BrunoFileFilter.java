46c4c8ff08d7e1962b2f278f68c5903d0b5e4ec4
Samuel Ainsworth <samuel_ainsworth@brown.edu>
2013-05-10 19:23:54 -0400

    Ignore *.class files

--- src/org/bruno/frontend/BrunoFileFilter.java
+++ src/org/bruno/frontend/BrunoFileFilter.java
@@ -1,19 +1,21 @@
 package org.bruno.frontend;
 
-import javax.swing.filechooser.FileFilter;
 import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
 
 public class BrunoFileFilter extends FileFilter {
 
-    @Override
-    public boolean accept(File f) {
-        return !f.isHidden() && !f.getName().endsWith(Bruno.FILE_EXT)
-                && !f.getName().endsWith("~");
-    }
+	@Override
+	public boolean accept(File f) {
+		return !f.isHidden() && !f.getName().endsWith(Bruno.FILE_EXT)
+				&& !f.getName().endsWith("~")
+				&& !f.getName().endsWith(".class");
+	}
 
-    @Override
-    public String getDescription() {
-        return "All Bruno files";
-    }
+	@Override
+	public String getDescription() {
+		return "All Bruno files";
+	}
 
 }
