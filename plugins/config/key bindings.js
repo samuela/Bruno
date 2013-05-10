ab223da80078799ae631b644bb7188d3ecb37eba
Michael Scheer <mgscheer@gmail.com>
2013-05-09 20:20:48 -0400

    key bindings script works

--- plugins/config/key bindings.js
+++ plugins/config/key bindings.js
@@ -1,4 +1,5 @@
 importPackage(Packages.javax.swing);
+importPackage(Packages.java.awt.event);
 //import some stuff
 
 //key is something like KeyEvent.VK_N for n
@@ -6,18 +7,18 @@ importPackage(Packages.javax.swing);
 //name is a string
 function bindKeyToName(key, mask, name){
     if (mask.equals("command")){
-	getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(key, Toolkit
+	bruno.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(key, Toolkit
 						.getDefaultToolkit().getMenuShortcutKeyMask()), name);
     }
     else if (mask.equals("control")){
-	getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(key, ActionEvent.CTRL_MASK()), name);
+	bruno.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(key, ActionEvent.CTRL_MASK), name);
     }
 }
 
 //name is a string
 //action is an action which will be called when the key binding is hit
 function bindNameToAction(name, action){
-    getRootPane().getActionMap().put(name, action);
+    bruno.getRootPane().getActionMap().put(name, action);
 }
 
 //scriptName is a string, the name of a script
