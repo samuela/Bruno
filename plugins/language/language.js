4fd9f8ab498df91446dfa057ec7f57d2462c628f
Michael Scheer <mgscheer@gmail.com>
2013-05-11 18:17:02 -0400

    added more languages

--- plugins/language/language.js
+++ plugins/language/language.js
@@ -1,7 +1,7 @@
 importClass(org.fife.ui.rsyntaxtextarea.SyntaxConstants);
 importClass(javax.swing.JOptionPane);
 
-var languages = ["Java", "C", "Scala", "Ruby", "Python", "XML", "Lisp", "LaTeX", "Perl", "bad language"];
+var languages = ["Java", "JavaScript", "Plain Text", "C", "Scala", "Ruby", "Python", "XML", "JSON", "Lisp", "LaTeX", "Perl", "C++", "CSS"];
 
 var choice = JOptionPane.showInputDialog(
                     null,
@@ -23,6 +23,12 @@ else if (choice.equals("Java")) {
     editingWindow.setSyntaxStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
     bruno.addJavaCompletion();
 }
+else if (choice.equals("JavaScript")) {
+    editingWindow.setSyntaxStyle(SyntaxConstants.SYNTAX_STYLE_JAVASCRIPT);
+}
+else if (choice.equals("Plain Text")) {
+    editingWindow.setSyntaxStyle(SyntaxConstants.SYNTAX_STYLE_NONE);
+}
 else if (choice.equals("C")) {
     editingWindow.setSyntaxStyle(SyntaxConstants.SYNTAX_STYLE_C);
 }
@@ -38,6 +44,9 @@ else if (choice.equals("Python")) {
 else if (choice.equals("XML")) {
     editingWindow.setSyntaxStyle(SyntaxConstants.SYNTAX_STYLE_XML);
 }
+else if (choice.equals("JSON")) {
+    editingWindow.setSyntaxStyle(SyntaxConstants.SYNTAX_STYLE_JSON);
+}
 else if (choice.equals("Lisp")){
    editingWindow.setSyntaxStyle(SyntaxConstants.SYNTAX_STYLE_LISP);
 }
@@ -46,6 +55,12 @@ else if (choice.equals("LaTeX")){
 }
 else if (choice.equals("Perl")){
    editingWindow.setSyntaxStyle(SyntaxConstants.SYNTAX_STYLE_PERL);
+}
+else if (choice.equals("C++")) {
+    editingWindow.setSyntaxStyle(SyntaxConstants.SYNTAX_STYLE_CPLUSPLUS);
+}
+else if (choice.equals("CSS")) {
+    editingWindow.setSyntaxStyle(SyntaxConstants.SYNTAX_STYLE_CSS);
 }
 else {
     JOptionPane.showMessageDialog(null, "Invalid language choice.", "Select a language", JOptionPane.ERROR_MESSAGE);
